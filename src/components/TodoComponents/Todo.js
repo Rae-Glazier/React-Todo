182423c9da35fd7881b88bd0cc42b0b5f0f2baa0
rae-glazier <glazier.home.life@gmail.com>
2020-03-10 11:32:16 -0500

    basic coded..just not working

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = props => {
+    return(
+        <div onClick={() => props.toggleCompleted(props.item.id)}
+        className={`item${props.item.completed ? " completed" : ""}`}>
+            <p>{props.todo}</p>
+        </div>
+    )
+}
+
+export default Todo;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        item: ""
+      };
+    }
+    handleChanges = e => {
+      this.setState({
+        item: e.target.value
+      });
+    };
+  
+
+    handleSubmit = e => {
+    //   e.preventDefault();
+      this.handleSubmit(this.state.item);
+      this.setState({
+        item: ""
+      });
+    };
+  
+    render() {
+      console.log("rendering form");
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="item"
+            value={this.state.item}
+            placeholder='...todo'
+            onChange={this.handleChanges}
+          />
+          <button type='submit'>Add</button>
+        </form>
+        
+      );
+    }
+  }
+  
+  export default TodoForm;
